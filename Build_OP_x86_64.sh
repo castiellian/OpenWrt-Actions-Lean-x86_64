08af85e2716d58dfa82a2ed1a861d18c3e5a4431
castiellian <51224589+castiellian@users.noreply.github.com>
2020-06-17 10:36:30 +0800

    Update Build_OP_x86_64.sh

--- Build_OP_x86_64.sh
+++ Build_OP_x86_64.sh
@@ -1,1 +1,76 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
 
+# 定制默认IP
+sed -i 's/192.168.1.1/192.168.1.8/g' package/base-files/files/bin/config_generate
+
+# 替换默认Argon主题
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+# 添加第三方软件包
+git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+
+
+
+#创建自定义配置文件 - OpenWrt-x86_64
+
+rm -f ./.config*
+touch ./.config
+
+#
+# ========================固件定制部分========================
+# 
+
+# 
+# 如果不对本区块做出任何编辑, 则生成默认配置固件. 
+# 
+
+# 以下为定制化固件选项和说明:
+#
+
+#
+# 有些插件/选项是默认开启的, 如果想要关闭, 请参照以下示例进行编写:
+# 
+#          =========================================
+#         |  # 取消编译VMware镜像:                   |
+#         |  cat >> .config <<EOF                   |
+#         |  # CONFIG_VMDK_IMAGES is not set        |
+#         |  EOF                                    |
+#          =========================================
+#
+
+# 
+# 以下是一些提前准备好的一些插件选项.
+# 直接取消注释相应代码块即可应用. 不要取消注释代码块上的汉字说明.
+# 如果不需要代码块里的某一项配置, 只需要删除相应行.
+#
+# 如果需要其他插件, 请按照示例自行添加.
+# 注意, 只需添加依赖链顶端的包. 如果你需要插件 A, 同时 A 依赖 B, 即只需要添加 A.
+# 
+# 无论你想要对固件进行怎样的定制, 都需要且只需要修改 EOF 回环内的内容.
+# 
+
+# 编译x86_64固件:
+cat >> .config <<EOF
+
+EOF
+# 
+# ========================固件定制部分结束========================
+# 
+
+
+sed -i 's/^[ \t]*//g' ./.config
+
+# 配置文件创建完成
