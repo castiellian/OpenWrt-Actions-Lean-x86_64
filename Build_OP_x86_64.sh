c783a060678ba93cd651373e994fde829b186931
castiellian <51224589+castiellian@users.noreply.github.com>
2020-06-17 10:38:25 +0800

    Update Build_OP_x86_64.sh

--- Build_OP_x86_64.sh
+++ Build_OP_x86_64.sh
@@ -7,7 +7,7 @@
 #=================================================
 
 # 定制默认IP
-sed -i 's/192.168.1.1/192.168.1.8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # 替换默认Argon主题
 rm -rf package/lean/luci-theme-argon
