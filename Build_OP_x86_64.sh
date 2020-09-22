9e5630ce08a9f9b83c5e7d21d10a3aa3cf6ad612
castiellian <51224589+castiellian@users.noreply.github.com>
2020-09-22 08:32:26 +0800

    Update Build_OP_x86_64.sh

--- Build_OP_x86_64.sh
+++ Build_OP_x86_64.sh
@@ -20,7 +20,7 @@ git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverc
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone https://github.com/kenzok8/openwrt-packages/luci-app-ssr-plus package/luci-app-ssr-plus
 git clone https://github.com/kenzok8/small package/small
 
 
