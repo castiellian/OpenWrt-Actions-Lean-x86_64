4d99c7de1977af798f232116677324d98c24299c
castiellian <51224589+castiellian@users.noreply.github.com>
2020-09-23 20:27:57 +0800

    Update Build_OP_x86_64.sh

--- Build_OP_x86_64.sh
+++ Build_OP_x86_64.sh
@@ -8,6 +8,8 @@
 
 # 定制默认IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
 
 # 替换默认Argon主题
 rm -rf package/lean/luci-theme-argon
