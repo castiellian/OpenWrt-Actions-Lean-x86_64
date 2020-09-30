e40ae5e1900c5d042d33c5d39e2752ebbea687be
castiellian <51224589+castiellian@users.noreply.github.com>
2020-09-30 10:07:18 +0800

    Update Build_OP_x86_64.sh

--- Build_OP_x86_64.sh
+++ Build_OP_x86_64.sh
@@ -259,7 +259,6 @@ CONFIG_PACKAGE_luci-app-eqos=y
 CONFIG_PACKAGE_luci-app-frpc=y
 CONFIG_PACKAGE_luci-app-gost=y
 # CONFIG_PACKAGE_luci-app-ipsec-vpnd is not set
-CONFIG_PACKAGE_luci-app-jd-dailybonus=y
 CONFIG_PACKAGE_luci-app-mwan3=y
 CONFIG_PACKAGE_luci-app-mwan3helper=y
 CONFIG_PACKAGE_luci-app-netdata=y
